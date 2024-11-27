fb40047f3b8265fe7e8111315b006e001f8df379
Keshav Shah <keshavrshah@gmail.com>
2024-11-27 08:55:30 -0500

    added scene manager

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 # imports
 import pygame
+from scene import Scene1
 
 # CONSTANTS
 WIDTH = 1280
 HEIGHT = 720
 TITLE = "FBLA 2025"
-BG_COLOR = pygame.Color(0, 0, 0)
 
 # pygame setup
 pygame.init()
@@ -14,15 +14,25 @@ pygame.display.set_caption(TITLE)
 clock = pygame.time.Clock()
 running = True
 
+# scenes
+active_scene = Scene1()
+
 while running:
+        # getting pressed keys and all events
+        keys = pygame.key.get_pressed()
+        events = []
         for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                         running = False
+                events.append(event)
 
-        screen.fill(BG_COLOR)
+        # Updating current scene with events, pressed keys, and scenes
+        active_scene.Update(events, keys, screen)
+
+        # Updating current scene; if no scene switch, then active_scene.next should be equal to active scene
+        active_scene = active_scene.next
 
         pygame.display.flip()
-
         clock.tick(60)
 
 pygame.quit()
