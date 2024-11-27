8ca49e3b0cbdcf8f3729dddad5a40c199e694ac5
Aarav <aarav.narayan@gmail.com>
2024-11-27 10:12:14 -0500

    added sprite class

--- main.py
+++ main.py
@@ -23,6 +23,13 @@ class Sprite(pygame.sprite.Sprite):
                 self.image=self.textures[tick%len(self.textures)]
                 screen.blit(self.image, (self.rect.x, self.rect.y))
 
+        def move(self, x, y, vx, vy):
+                # if self.rect.x > x-(x%vx):
+                #         vx *= -1
+                # if self.rect.y > y-(y%vy):
+                #         vy *= -1
+                self.rect.move(vx, vy)
+
         def update(self):
                 pass
 
@@ -34,6 +41,27 @@ clock = pygame.time.Clock()
 running = True
 tick = 0
 
+test_textures = [
+        pygame.image.load(os.path.join('test', 'sprite_00.png')),
+        pygame.image.load(os.path.join('test', 'sprite_01.png')),
+        pygame.image.load(os.path.join('test', 'sprite_02.png')),
+        pygame.image.load(os.path.join('test', 'sprite_03.png')),
+        pygame.image.load(os.path.join('test', 'sprite_04.png')),
+        pygame.image.load(os.path.join('test', 'sprite_05.png')),
+        pygame.image.load(os.path.join('test', 'sprite_06.png')),
+        pygame.image.load(os.path.join('test', 'sprite_07.png')),
+        pygame.image.load(os.path.join('test', 'sprite_08.png')),
+        pygame.image.load(os.path.join('test', 'sprite_09.png')),
+        pygame.image.load(os.path.join('test', 'sprite_10.png')),
+        pygame.image.load(os.path.join('test', 'sprite_11.png')),
+        pygame.image.load(os.path.join('test', 'sprite_12.png')),
+        pygame.image.load(os.path.join('test', 'sprite_13.png')),
+        pygame.image.load(os.path.join('test', 'sprite_14.png')),
+        pygame.image.load(os.path.join('test', 'sprite_15.png'))
+]
+
+test_sprite = Sprite(test_textures, 100, 100)
+
 #GAME LOOP
 while running:
         for event in pygame.event.get():
@@ -41,6 +69,17 @@ while running:
                         running = False
 
         screen.fill(BG_COLOR)
+
+        dest_x = random.randint(0, WIDTH)
+        dest_y = random.randint(0, HEIGHT)
+        
+        if test_sprite.rect.x == dest_x and test_sprite.rect.y == dest_y:
+                dest_x = random.randint(0, WIDTH)
+                dest_y = random.randint(0, HEIGHT)
+                print("New Destination")
+
+        test_sprite.move(dest_x, dest_y, 1, 1)
+        test_sprite.show()
 
         pygame.display.flip()
 
